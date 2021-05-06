6bc1bd070900a754994a33aa3e06b4a1d0c43d3f
AlitalipSever <alitalipsever@gmail.com>
2021-05-06 23:15:02 +0200

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by 7mobiledev on 6.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
